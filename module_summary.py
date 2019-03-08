de29b1db77a68af67c5f4a898961a148ca3ccd6d
Dyfeomorfizm <pawel.zawadzki@aol.com>
2019-03-09 00:29:47 +0100

    wrong blank lines fixed

--- module_summary.py
+++ module_summary.py
@@ -1,12 +1,15 @@
 import sys
 
+
 def get_first_paragraph(text):
     paragraphs = text.split("\n\n")
     return paragraphs[0]
 
+
 def do_import(module_name):
     module = __import__(module_name)
     return module
+
 
 def create_doc(module):
     doc = module.__name__ + "\n" + get_first_paragraph(module.__doc__)
@@ -20,13 +23,15 @@ def create_doc(module):
             print(e)
     return doc
 
+
 def to_file(filename, doc):
     with open(filename, 'w') as f:
         f.write(doc)
 
-def main(module_name, filename):
+
+def run(module_name, filename):
     to_file(filename, create_doc(do_import(module_name)))
 
+
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
-    
+    run(sys.argv[1], sys.argv[2])
